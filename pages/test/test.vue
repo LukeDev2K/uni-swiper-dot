23544eefe991df2c2dfd98a72e8b176754034307
Long <1244252454@qq.com>
2021-05-19 16:37:10 +0800

    添加测试页

---
+++ pages/test/test.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<button type="default" @click="openScan">打开扫一扫</button>
+		<button type="default" @click="openMiniprogram">拉起小程序</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			openScan: function() {
+				uni.scanCode({
+					scanType:['qrCode'],
+					success: (res) => {
+						console.log(JSON.stringify(res))
+					},
+					fail: (err) => {
+						console.error(JSON.stringify(err))
+					}
+				})
+			},
+			openMiniprogram: function() {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
