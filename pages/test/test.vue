b8ce739d7f19002a0897e15c47d90bf07e9fcf07
Long <1244252454@qq.com>
2021-05-19 17:49:15 +0800

    添加拉起小程序代码

--- pages/test/test.vue
+++ pages/test/test.vue
@@ -1,7 +1,8 @@
 <template>
 	<view>
-		<button type="default" @click="openScan">打开扫一扫</button>
-		<button type="default" @click="openMiniprogram">拉起小程序</button>
+		<button class="btn-test" @click="openScan">打开扫一扫</button>
+		<button class="btn-test" @click="openMiniprogram">拉起小程序</button>
+		<text class="qr-content">{{qrContent}}</text>
 	</view>
 </template>
 
@@ -9,7 +10,7 @@
 	export default {
 		data() {
 			return {
-				
+				qrContent:''
 			};
 		},
 		methods:{
@@ -18,19 +19,41 @@
 					scanType:['qrCode'],
 					success: (res) => {
 						console.log(JSON.stringify(res))
+						this.qrContent = res.result
 					},
 					fail: (err) => {
 						console.error(JSON.stringify(err))
+						this.qrContent = '扫码失败==>' + JSON.stringify(err)
 					}
 				})
 			},
 			openMiniprogram: function() {
-				
+				let shares = {}  
+				plus.share.getServices(item => {  
+				    for(let i in item) {  
+				        const t = item[i]  
+				        shares[t.id] = t  
+				    }  
+				    shares['weixin'].launchMiniProgram({  
+				        id: 'gh_8c3933c43727', // 小程序原始ID  
+				        type: 0, // 正式版:0，测试版:1，体验版:2  
+				        path: 'pages/index/index'  
+				    })  
+				    console.log(shares)  
+				},err => {  
+				    console.log(err.message)  
+				}) 
 			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	.btn-test {
+		margin-bottom: 40rpx;
+	}
+	
+	.qr-content {
+		width: 100%;
+	}
 </style>
